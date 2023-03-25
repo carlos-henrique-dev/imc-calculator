a4994892397543e782ceebf4fe3b033aa05f333e
Carlos Henrique <henriqueok20@gmail.com>
2023-03-24 20:59:29 -0400

    adds readme and refactor calculator

---
+++ README.md
@@ -0,0 +1,8 @@
+# IMC Calculator
+
+This is a simple IMC calculator made with NodeJS as an exercise for the class of Configuration and Management of Software Systems.
+
+## How to use
+
+1. Clone the repository
+2. Run `npm run calculate` to see the results
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-function imcCalculator(weight, height) {
+function imcCalculator({ weight, height }) {
 	const imc = weight / height ** 2;
 	return imc;
 }
 
-const weight = 84;
-const height = 1.88;
+const imc1 = imcCalculator({ weight: 84, height: 1.88 });
+const imc2 = imcCalculator({ weight: 64, height: 1.60 });
+const imc3 = imcCalculator({ weight: 94, height: 1.98 });
 
-const imc = imcCalculator(weight, height);
-
-console.log(imc);
+console.log('IMC 1:', imc1);
+console.log('IMC 2:', imc2);
+console.log('IMC 3:', imc3);
